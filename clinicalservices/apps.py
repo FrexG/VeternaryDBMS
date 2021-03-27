a0f626d9bcea38eb3209beeb6910a3bc2c0aa814
FrexG <fraolgela@gmail.com>
2021-03-27 23:14:30 +0300

    URL config for all apps

---
+++ clinicalservices/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ClinicalservicesConfig(AppConfig):
+    name = 'clinicalservices'
---
+++ vaccineandparasite/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VaccineandparasiteConfig(AppConfig):
+    name = 'vaccineandparasite'
