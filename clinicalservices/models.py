45e8eecdf04bba959e1a7e4920a031be7b21a8be
FrexG <fraolgela@gmail.com>
2021-04-08 14:07:50 +0300

    Addition of #AI_Service model

--- clinicalservices/models.py
+++ clinicalservices/models.py
@@ -13,6 +13,35 @@ class ClinicalService(models.Model):
     service_date = models.DateField(
         "Service Date", auto_now=False, auto_now_add=True)
 
+# Artificial Insemination model
+
+
+class AIService(models.Model):
+
+    case_number = models.ForeignKey(
+        Customer, verbose_name="Case Number", on_delete=models.CASCADE)
+    # Service type, this will be prefield by service type = AI
+    service_type = models.ForeignKey(
+        Service, verbose_name="Service Type", on_delete=models.CASCADE)
+
+    service_date = models.DateField(
+        "Service Date", auto_now=False, auto_now_add=True)
+
+    last_calving_date = models.DateField(verbose_name="Last Calving Date")
+
+    color = models.CharField(verbose_name="Color", max_length=20)
+
+    ai_frequency = models.CharField(verbose_name="AI Frequency", max_length=30)
+
+    bull_number = models.PositiveBigIntegerField(verbose_name="Bull Number")
+
+    pd_result = models.CharField(verbose_name="PD Result", max_length=50)
+
+    quantity = models.PositiveIntegerField(verbose_name="Quantity")
+
+    # Quantity of Services
+    quantity = models.PositiveIntegerField("Quantity")
+
 
 class ServiceProvided(models.Model):
     # Services Table
@@ -21,6 +50,3 @@ class ServiceProvided(models.Model):
     # Many to One r/n ship with ClinicalService Table
     clinical_service = models.ForeignKey(
         ClinicalService, verbose_name="Service ID", on_delete=models.CASCADE)
-
-    # Quantity of Services
-    quantity = models.PositiveIntegerField("Quantity")
