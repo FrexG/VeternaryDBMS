0595ac2a6a47c992e913b0d30d74e411eac014f5
FrexG <fraolgela@gmail.com>
2021-04-26 09:30:23 +0300

    added home to urls path

--- VeternaryDBMS/urls.py
+++ VeternaryDBMS/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('home.urls')),
     path('register', include('registernewuser.urls')),
     path('regular', include('regulartreatedanimals.urls')),
     path('clinicalservice', include('clinicalservices.urls')),
