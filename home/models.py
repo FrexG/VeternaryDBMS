2cfc6492c9a11fcfa8f19de5ef8d146198ad89b7
FrexG <fraolgela@gmail.com>
2021-06-09 13:29:30 +0300

    Profile model for defining role

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+# profile model
+# for specifying Role of each user
+
+USER_ROLES = [('Admin', 'Adminstrator'),
+              ('Cashier', 'Cashier'), ('Vet', 'Veternarian')]
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(choices=USER_ROLES, max_length=100)
+
+    def __str__(self):
+        return f"{self.user}:{self.role}"
