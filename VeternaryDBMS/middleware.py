6488c0666fd3524f20565a0d95916700776d13d4
FrexG <fraolgela@gmail.com>
2021-06-10 14:53:39 +0300

    role based access completed

--- VeternaryDBMS/middleware.py
+++ VeternaryDBMS/middleware.py
@@ -1,31 +1,31 @@
 from home.models import Profile
 from django.shortcuts import redirect
 
-VET_ACCESS = ['regular', 'clinicalservice', 'parasite&vaccination']
-CASHIER_ACCESS = ['register']
-ADMIN_ACCESS = VET_ACCESS + CASHIER_ACCESS
-
 
 class GetUserMiddleware:
+    VET_ACCESS = ['regular', 'clinicalservice',
+                  'parasite&vaccination', 'logout']
+    CASHIER_ACCESS = ['register', 'logout']
+    ADMIN_ACCESS = VET_ACCESS + CASHIER_ACCESS
+
     def __init__(self, get_response):
         self.get_response = get_response
-        print(get_response)
 
     def __call__(self, request):
-        if str(request.user) != "AnonymousUser":
-            self.__process_permission(request)
         response = self.get_response(request)
         return response
 
-    def __process_permission(self, request):
-        # get role of each loged in user
-        role = Profile.objects.get(user=request.user).role
-        path = request.path.strip('/')
+    def process_view(self, request, view_func, *args, **kwargs):
+        # get role of each loged in use
 
-        if str(role) == 'Vet':
-            print(str(role))
-            if path in VET_ACCESS:
-                print("Can access")
-            else:
-                return redirect("/")
-        print(path)
+        if str(request.user) != "AnonymousUser":
+            role = Profile.objects.get(user=request.user).role
+            path = request.path.strip('/')
+
+            # If user is logged in as a Veternerian
+            if str(role) == 'Vet' and path not in self.VET_ACCESS:
+                return redirect("/regular")
+            # If user is logged in as a Cashier
+            if str(role) == 'Cashier' and path not in self.CASHIER_ACCESS:
+                return redirect("/register")
+            # grant the admin access to all pages
